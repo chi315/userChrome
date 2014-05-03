ef01a56dabde0102059f64b5fa721f6731c08839
Drager-oos <Drager-oos@users.noreply.github.com>
2014-05-03 11:58:18 +0800

    Update SearchbarBtnPlus.uc.js

--- FunctionsPlus/SearchbarBtnPlus.uc.js
+++ FunctionsPlus/SearchbarBtnPlus.uc.js
@@ -43,36 +43,6 @@
 		}
 	};
 
-	var searchInput = document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-textbox");
-	searchInput.setAttribute("onDOMMouseScroll", "FindScroll.onScroll(event);");
-	searchInput.addEventListener('dblclick', function (event) {
-		if (event.button == 0) {
-			searchbar.value = "";
-			gWHT.destroyToolbar();
-			gFindBar.close();
-		}
-		event.preventDefault();
-	}, false);
-	FindScroll = {
-		onScroll: function(event) {
-			if (event.detail > 0) {
-				if (searchbar.value == "") {return;}
-				gWHT.addWord(searchbar.value);
-				gWHT.find(searchbar.value, false);
-//				gFindBar._findField.value = searchbar.value;gFindBar.open();gFindBar.toggleHighlight(1);
-//				gFindBar.onFindAgainCommand(false);
-			}
-			else {
-				if (searchbar.value == "") {return;}
-				gWHT.addWord(searchbar.value);
-				gWHT.find(searchbar.value, true);
-//				gFindBar._findField.value = searchbar.value;gFindBar.open();gFindBar.toggleHighlight(1);
-//				gFindBar.onFindAgainCommand(true);
-			}
-			return;
-		}
-	};
-
 	var searchbarEngineBtn = document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-engine-button");
 	searchbarEngineBtn.setAttribute("onDOMMouseScroll", "EngineScroll.onScroll(event);");
 	searchbarEngineBtn.setAttribute("tooltiptext","左鍵：搜尋選單\n雙擊左鍵：重設至默認搜尋引擎\n中鍵：Google翻譯搜尋欄關鍵字\n　　　貼上就 Google 翻譯 (新分頁前景)\n滾動：切換搜索引擎\n右鍵：尋找 & WordHighlightToolbar高亮搜尋欄關鍵字\n　　　貼上就尋找 & WordHighlightToolbar高亮");
@@ -108,5 +78,58 @@
 	}, false);
 	EngineScroll = {
 		onScroll: function(event) {searchbar.selectEngine(event, (event.detail > 0));}
+	};
+
+	var searchInput = document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-textbox");
+
+	var findMatchCase = document.createElement("checkbox");
+	findMatchCase.id = "findMatchCase";
+	findMatchCase.setAttribute("tooltiptext", "左鍵：符合大小寫\n右鍵：關閉尋找欄\n向上滾動：尋找上一筆\n向下滾動：尋找下一筆");
+	findMatchCase.setAttribute("accesskey", "C");
+	findMatchCase.setAttribute("oncommand", 'gFindBar._setCaseSensitivity(this.checked);document.getElementById("searchbar").focus();');
+	findMatchCase.setAttribute('onclick', 'if (event.button == 2) {gFindBar.close();document.getElementById("searchbar").value="";event.preventDefault();}');
+	findMatchCase.setAttribute("onDOMMouseScroll", "FindScroll.onScroll(event);");
+	searchInput.appendChild(findMatchCase);
+	
+	FindScroll = {
+		onScroll: function(event) {
+			if (event.detail > 0) {
+				if (searchbar.value == "") {return;}
+				gFindBar._findField.value = searchbar.value;gFindBar.open();gFindBar.toggleHighlight(1);
+				gFindBar.onFindAgainCommand(false);
+			}
+			else {
+				if (searchbar.value == "") {return;}
+				gFindBar._findField.value = searchbar.value;gFindBar.open();gFindBar.toggleHighlight(1);
+				gFindBar.onFindAgainCommand(true);
+			}
+			return;
+		}
+	};
+
+	var gWHTFindBtn = document.createElement("toolbarbutton");
+	gWHTFindBtn.setAttribute("id", "gWHTFind-button");
+	gWHTFindBtn.setAttribute("type", "button");
+	gWHTFindBtn.setAttribute("class", "toolbarbutton-1");
+	gWHTFindBtn.setAttribute("tooltiptext", "左鍵：關閉WordHighlightToolbar\n向上滾動：尋找上一筆\n向下滾動：尋找下一筆");
+	gWHTFindBtn.style.listStyleImage = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAANUlEQVQ4jWNgGBTg6dOi/6RgrAb8/19PFB7EBlAUBoMDFD0t+k8qxjCgngQ4SA2gKAwGDAAAM3SE/usVkKQAAAAASUVORK5CYII=)";
+	gWHTFindBtn.setAttribute("oncommand", 'gWHT.destroyToolbar();document.getElementById("searchbar").value="";');
+	gWHTFindBtn.setAttribute("onDOMMouseScroll", "gWHTFindScroll.onScroll(event);");
+	searchInput.appendChild(gWHTFindBtn);
+
+	gWHTFindScroll = {
+		onScroll: function(event) {
+			if (event.detail > 0) {
+				if (searchbar.value == "") {return;}
+				gWHT.addWord(searchbar.value);
+				gWHT.find(searchbar.value, false);
+			}
+			else {
+				if (searchbar.value == "") {return;}
+				gWHT.addWord(searchbar.value);
+				gWHT.find(searchbar.value, true);
+			}
+			return;
+		}
 	};
 }());
