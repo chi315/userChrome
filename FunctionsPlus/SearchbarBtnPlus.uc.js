d66043296002b406e0c7f50e6a7337452aa7c559
Drager-oos <Drager-oos@users.noreply.github.com>
2014-05-04 11:55:21 +0800

    Update SearchbarBtnPlus.uc.js

--- FunctionsPlus/SearchbarBtnPlus.uc.js
+++ FunctionsPlus/SearchbarBtnPlus.uc.js
@@ -87,7 +87,7 @@
 	findMatchCase.setAttribute("tooltiptext", "左鍵：符合大小寫\n右鍵：關閉高亮顯示及清除關鍵字\n向上滾動：尋找上一筆\n向下滾動：尋找下一筆");
 	findMatchCase.setAttribute("accesskey", "C");
 	findMatchCase.setAttribute("oncommand", 'gFindBar._setCaseSensitivity(this.checked);document.getElementById("searchbar").focus();');
-	findMatchCase.setAttribute('onclick', 'if (event.button == 2) {gFindBar.toggleHighlight(0);document.getElementById("searchbar").value="";event.preventDefault();}');
+	findMatchCase.setAttribute('onclick', 'if (event.button == 2) {gFindBar.toggleHighlight(0);document.getElementById("searchbar").value="";gFindBar._foundMatches.hidden = true;event.preventDefault();}');
 	findMatchCase.setAttribute("onDOMMouseScroll", "FindScroll.onScroll(event);");
 	searchInput.appendChild(findMatchCase);
 	
