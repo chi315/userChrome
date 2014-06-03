e1fd1e1e9e282f012c857b01e204a289771cd960
Drager-oos <Drager-oos@users.noreply.github.com>
2014-06-03 13:46:57 +0800

    Update AllTabsBtnPlus.uc.js

--- Mozest-Backups/AllTabsBtnPlus.uc.js
+++ Mozest-Backups/AllTabsBtnPlus.uc.js
@@ -1,22 +1,11 @@
 (function() {
-	var AllTabsBtn = document.getElementById("alltabs-button");
-	AllTabsBtn.setAttribute("onDOMMouseScroll", "ReloadOrStopScroll.onScroll(event);");
-	AllTabsBtn.style.listStyleImage = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAYklEQVQ4je3SMQ6CQAAF0ReNVGojFNp5Aag9HB23oOKc7EYbNyZWu4RyJ5nyT/X5cUGX6dkfEyLemQaMadwWjpMrrvDcME7ea6AGdgs8bLtywA2OWAojETMOvjQY8Mq0xwk+lZbHj/TRZdcAAAAASUVORK5CYII=)";
-	AllTabsBtn.setAttribute("tooltiptext","左鍵：分頁選單\n雙擊左鍵：重新載入此分頁\n中鍵：關閉其他分頁\n右鍵：停止載入此分頁\n向上滾動：停止載入所有分頁\n向下滾動：重新載入所有分頁");
+	var AllTabsBtn = $("urlbar").appendChild($("alltabs-button", {
+		onDOMMouseScroll: "ReloadOrStopScroll.onScroll(event);",
+		tooltiptext: "左鍵：所有分頁選單\n中鍵：復原分頁選單\n右鍵：分頁選單\n向上滾動：停止載入所有分頁\n向下滾動：重新載入所有分頁\n\n在復原分頁選單：\n右鍵：清除復原分頁列表",
+	}));
 	AllTabsBtn.addEventListener("click", function(event) {
-		if (event.button == 1) {
-			gBrowser.removeAllTabsBut(gBrowser.mCurrentTab);
-		}
-		else if (event.button == 2) {
-			BrowserStop();
-		}
-		event.preventDefault();
-	}, false);
-	AllTabsBtn.addEventListener('dblclick', function (event) {
-		if (event.button == 0) {
-			gBrowser.mCurrentBrowser.reload();
-		}
-		event.preventDefault();
+		if (event.button == 1) {$("undoCloseTabPopup").openPopupAtScreen(event.screenX, event.screenY, true);}
+		else if (event.button == 2) {$("tabContextMenu").openPopupAtScreen(event.screenX, event.screenY, true); event.preventDefault();}
 	}, false);
 	ReloadOrStopScroll = {
 		onScroll: function(event) {
@@ -25,7 +14,69 @@
 			return;
 		}
 	};
-	var AllTabsPopup = document.getElementById("alltabs-popup");
-	AllTabsPopup.setAttribute('onclick', 'event.preventDefault(); event.stopPropagation();');
-	AllTabsPopup.setAttribute('ondblclick', 'event.preventDefault(); event.stopPropagation();');
+	$("alltabs-popup", {onclick: "event.preventDefault(); event.stopPropagation();",});
+
+	var css = '\
+		#main-window {counter-reset:tabs;}\
+		.tabbrowser-tab:not(:-moz-any([pinned],[concealed])) {counter-increment:tabs;}\
+		/* Alltabs-button 顯示標籤頁總數 */\
+		#alltabs-button:after {\
+		content:counter(tabs);\
+		display:-moz-box;\
+		-moz-box-pack:center;\
+		color:#000;\
+		font-weight:bold;\
+		font:18px Arial!important;\
+		min-width:17px;\
+		}\
+		#alltabs-button > :-moz-any(label, image) {display:none;}\
+		#alltabs-button {\
+		visibility:visible!important;\
+		padding:0px!important;\
+		}\
+		/* 彈出菜單顯示標籤頁序號 */\
+		#alltabs-popup {counter-reset:latabs;}\
+		#alltabs-popup .menuitem-iconic.alltabs-item.menuitem-with-favicon {counter-increment:latabs;}\
+		#alltabs-popup .menuitem-iconic.alltabs-item.menuitem-with-favicon:before {content:counter(latabs);}\
+		/* 彈出菜單顯示關閉標籤頁序號 */\
+		#undoCloseTabPopup .menuitem-iconic {counter-increment:menuitem;}\
+		#undoCloseTabPopup menuitem:before {content:counter(menuitem);}\
+		\
+		#alltabs-popup .menuitem-iconic.alltabs-item.menuitem-with-favicon:before,\
+		#undoCloseTabPopup menuitem:before {\
+		display:-moz-box;\
+		-moz-box-pack:center;\
+		font-weight:bold;\
+		min-width:26px;\
+		}\
+		'.replace(/[\r\n\t]/g, '');;
+	AllTabsBtn.style = addStyle(css);
+
+	var popup = $("mainPopupSet").appendChild($C("menupopup", {
+		id: "undoCloseTabPopup",
+		onpopupshowing: "this.populateUndoSubmenu();",
+		tooltip: "bhTooltip",
+		popupsinherittooltip: "true",
+		onclick: "if (event.button == 2) {ClearUndoCloseTabList.setPref('browser.sessionstore.max_tabs_undo', 'int', 0); ClearUndoCloseTabList.setPref('browser.sessionstore.max_tabs_undo', 'int', 50); event.preventDefault();}",
+	}));
+	popup.populateUndoSubmenu = eval("(" + HistoryMenu.prototype.populateUndoSubmenu.toString().replace("undoMenu.firstChild", "this").replace(/.*undoMenu.*/g, "") + ")");
+
+	function $(id, doc) (doc || document).getElementById(id);
+	function $(id, attr) {
+		var el = document.getElementById(id);
+		if (attr) Object.keys(attr).forEach(function(n) el.setAttribute(n, attr[n]));
+		return el;
+	}
+	function $C(name, attr) {
+		var el = document.createElement(name);
+		if (attr) Object.keys(attr).forEach(function(n) el.setAttribute(n, attr[n]));
+		return el;
+	}
+	function addStyle(css) {
+		var pi = document.createProcessingInstruction(
+			'xml-stylesheet',
+			'type="text/css" href="data:text/css;utf-8,' + encodeURIComponent(css) + '"'
+		);
+		return document.insertBefore(pi, document.documentElement);
+	}
 })();
