2d4c485af3a586451e46b8686b804f37769c4821
Drager-oos <Drager-oos@users.noreply.github.com>
2014-06-18 10:34:02 +0800

    Update ShowDateAndTime.uc.js

--- FunctionsPlus/ShowDateAndTime.uc.js
+++ FunctionsPlus/ShowDateAndTime.uc.js
@@ -2,11 +2,11 @@
 	DaT = $("TabsToolbar").appendChild($C("label", {
 				id: "Clock",
 				tooltiptext: "現時日期和時間",
-				style: "margin: 5px -4px 5px 4px; font-weight:bold; font:18px Arial; color: grey; min-width:244px;",
+				style: "margin: 5px -4px 5px 4px; font-weight: bold; font: 18px Arial, Microsoft Yahei; color: grey; min-width: 210px;",
 				ordinal: "0",
 			}));
 	function startTime() {
-		var dayName = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+		var dayName = new Array ("週日", "週一", "週二", "週三", "週四", "週五", "週六");
 		var today = new Date();
 		var D = today.getDate(),
 			M = today.getMonth() + 1,
@@ -20,7 +20,7 @@
 		m = checkTime(m);
 		s = checkTime(s);
 		DaT.value = D + "/" + M + "/" + Y + ", " + dayName[today.getDay()] + " " + h + ":" + m + ":" + s;
-		setTimeout(function() {startTime();}, 0);
+		setTimeout(function() {startTime();}, 250);
 	}
 	function checkTime(i) {
 		if (i < 10) {i = "0" + i}
