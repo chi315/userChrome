1f602097e550383470c81a61fc2c2fcae8588851
Drager-oos <Drager-oos@users.noreply.github.com>
2014-07-01 14:36:31 +0800

    Update and rename FunctionsPlus/ShowDateAndTime.uc.js to BtnPlus/ShowDateAndTime.uc.js

--- BtnPlus/ShowDateAndTime.uc.js
+++ BtnPlus/ShowDateAndTime.uc.js
@@ -6,6 +6,8 @@
 				ordinal: "0",
 			}));
 	function startTime() {
+//		var dayName = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+//		var dayName = new Array ("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
 		var dayName = new Array ("週日", "週一", "週二", "週三", "週四", "週五", "週六");
 		var today = new Date();
 		var D = today.getDate(),
@@ -20,7 +22,7 @@
 		m = checkTime(m);
 		s = checkTime(s);
 		DaT.value = D + "/" + M + "/" + Y + ", " + dayName[today.getDay()] + " " + h + ":" + m + ":" + s;
-		setTimeout(function() {startTime();}, 250);
+		setTimeout(function() {startTime();}, 100);
 	}
 	function checkTime(i) {
 		if (i < 10) {i = "0" + i}
