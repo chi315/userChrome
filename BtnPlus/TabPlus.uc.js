dfbe6c21622eff45db995de9f68838a94db72d10
Drager-oos <Drager-oos@users.noreply.github.com>
2014-07-04 19:16:44 +0800

    Update TabPlus.uc.js

--- BtnPlus/TabPlus.uc.js
+++ BtnPlus/TabPlus.uc.js
@@ -21,26 +21,46 @@
 		"if(/^javascript:/.test(url)||isTabEmpty(gBrowser.selectedTab)) {loadCurrent();} else {this.handleRevert();gBrowser.loadOneTab(url, {postData: postData, inBackground: false, allowThirdPartyFixup: true});}"));
 	} catch(e) {}
 
-	// 滾輪：切換分頁
-	gBrowser.mTabContainer.addEventListener("DOMMouseScroll", function(e) {
-		this.advanceSelectedTab(e.detail > 0 ? +1 : -1, true);
+	// 向上滾輪：關閉重複分頁 & 向下滾輪：關閉其他分頁 & 滾輪：切換分頁
+	gBrowser.mTabContainer.addEventListener('DOMMouseScroll', function(e) {
+		if (e.target.localName == "tab") {
+			if (e.detail > 0) {
+				gBrowser.removeAllTabsBut(gBrowser.mCurrentTab);
+			}
+			else {
+				CloseRepeatedTabs();
+			}
+			return;
+		}
+		else {
+			this.advanceSelectedTab(e.detail > 0 ? +1 : -1, true);
+		}
+		e.stopPropagation();
+		e.preventDefault();
+		return;
 	}, true);
-	
-	// 雙擊左鍵：重新載入
-	gBrowser.mTabContainer.addEventListener('dblclick', function (e) {
-		if (e.target.localName == 'tab' && e.button == 0) {
-			getBrowser().getBrowserForTab(e.target).reload();
-		}
-	}, false);
 
-	// 右鍵：復原已關閉分頁
+	// 中鍵：復原已關閉分頁 & 左鍵：Gmail
 	gBrowser.mTabContainer.addEventListener("click", function(e) {
-		if (e.target.localName == "tab" && e.button == 2) {
-			undoCloseTab();
-			e.preventDefault();
-			e.stopPropagation();
+		if (e.target.localName == "tab") {
+			if (e.button == 2) {
+				document.getElementById('tabContextMenu').openPopupAtScreen(e.screenX, e.screenY, true);
+			}
 		}
-	}, false);
+		else {
+			if (e.button == 2 && !e.ctrlKey) {
+				gBrowser.selectedTab = gBrowser.addTab('https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/?tab%3Dwm&scc=1&ltmpl=default&ltmplcache=2');
+				e.preventDefault();
+				e.stopPropagation();
+			}
+			else if (e.button == 1) {
+				undoCloseTab();
+				e.preventDefault();
+				e.stopPropagation();
+			}
+		}
+		return;
+	}, true);
 
 	// 鼠標停留分頁自動聚焦
 	(document.getElementById("tabbrowser-tabs") || gBrowser.mTabBox).addEventListener('mouseover',
