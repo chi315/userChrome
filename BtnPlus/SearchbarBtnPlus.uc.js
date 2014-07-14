75735272ea81ed18cf29f02ea2ae8a6d3d2f264d
Drager-oos <Drager-oos@users.noreply.github.com>
2014-07-14 17:23:12 +0800

    Update SearchbarBtnPlus.uc.js

--- BtnPlus/SearchbarBtnPlus.uc.js
+++ BtnPlus/SearchbarBtnPlus.uc.js
@@ -7,18 +7,11 @@
 自動清除搜索欄關鍵字並切換至默認搜索引擎
 
 searchbar-engine-button
-在按鈕上
 左鍵：搜尋選單
 雙擊左鍵：重設至默認搜索引擎
 滾動：切換搜索引擎
 中鍵：貼上就瀏覽 (新分頁背景)
 右鍵：貼上就瀏覽 (新分頁前景)
-
-在選單上
-選擇指定搜索引擎後即搜尋 (新分頁前景)
-❖ 若搜索欄有關鍵字，便搜尋搜索欄關鍵字
-❖ 若搜索欄沒有關鍵字並選取了文字，便搜尋選取文字
-❖ 否則便貼上就搜尋
 
 findMatchCase
 左鍵：符合大小寫
@@ -35,15 +28,23 @@ gWHTFind-button
 向下滾動：尋找下一筆
 
 SearchEngine-button
+在選單上
+選擇指定搜索引擎後即搜尋 (新分頁前景)
+❖ 若搜索欄有關鍵字，便搜尋搜索欄關鍵字
+❖ 若搜索欄沒有關鍵字並選取了文字，便搜尋選取文字
+❖ 否則便貼上就搜尋
+
+在按鈕上
 左鍵：Google 加密
 中鍵：Google 翻譯
 右鍵：Google 加密站內
 向上滾動：百度圖片
 向下滾動：Google 圖片
 
-❖ 若搜索欄有關鍵字，便搜尋搜索欄關鍵字
-❖ 若搜索欄沒有關鍵字並選取了文字，便搜尋選取文字
-❖ 否則便貼上就搜尋
+❖ 搜尋搜索欄關鍵字
+❖ 搜尋選取文字
+❖ 貼上就搜尋
+❖ 新分頁前景
  **** 結束說明 ****/
 
 (function() {
@@ -63,15 +64,15 @@ SearchEngine-button
 	};
 
 	var searchPopup = document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-popup");
-	searchPopup.setAttribute("onclick", "if (event.button !== 0) {event.preventDefault(); event.stopPropagation();}");
+	searchPopup.setAttribute("onclick", "event.preventDefault(); event.stopPropagation();");
 	var searchGoBtn = document.getAnonymousElementByAttribute(searchbar, "anonid", "search-go-button");
 	var searchInput = document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-textbox");
 
 	var searchbarEngineBtn = document.getAnonymousElementByAttribute(searchbar, "anonid", "searchbar-engine-button");
-	searchbarEngineBtn.setAttribute("onclick", "if (event.button == 0) {SearchEngine.popupClick(event);} else if (event.button == 1) {gBrowser.addTab(readFromClipboard());} else if (event.button == 2) {gBrowser.selectedTab = gBrowser.addTab(readFromClipboard()); event.preventDefault();}");
+	searchbarEngineBtn.setAttribute("onclick", "if (event.button == 1) {gBrowser.addTab(readFromClipboard());} else if (event.button == 2) {gBrowser.selectedTab = gBrowser.addTab(readFromClipboard()); event.preventDefault();}");
 	searchbarEngineBtn.setAttribute("ondblclick", "if (event.button == 0) {Reset.onClick(event); event.preventDefault();}");
 	searchbarEngineBtn.setAttribute("onDOMMouseScroll", "EngineScroll.onScroll(event);");
-	searchbarEngineBtn.setAttribute("tooltiptext","在按鈕上\n左鍵：搜尋選單\n雙擊左鍵：清除搜索欄關鍵字\n　　　　　重設至默認搜索引擎\n滾動：切換搜索引擎\n中鍵：貼上就瀏覽 (新分頁背景)\n右鍵：貼上就瀏覽 (新分頁前景)\n\n在選單上\n選擇指定搜索引擎後即搜尋 (新分頁前景)\n❖ 若搜索欄有關鍵字，便搜尋搜索欄關鍵字\n❖ 若搜索欄沒有關鍵字並選取了文字，便搜尋選取文字\n❖ 否則便貼上就搜尋");
+	searchbarEngineBtn.setAttribute("tooltiptext","左鍵：搜尋選單\n雙擊左鍵：清除搜索欄關鍵字\n　　　　　重設至默認搜索引擎\n滾動：切換搜索引擎\n中鍵：貼上就瀏覽 (新分頁背景)\n右鍵：貼上就瀏覽 (新分頁前景)");
 	EngineScroll = {
 		onScroll: function(event) {searchbar.selectEngine(event, (event.detail > 0));}
 	};
@@ -139,6 +140,7 @@ SearchEngine-button
 		oncommand: "gWHT.GET_KEYWORD = !gWHT.GET_KEYWORD",
 		onclick: "if (event.button == 2) {gWHT.destroyToolbar(); document.getElementById('searchbar').value=''; event.preventDefault();}",
 		onDOMMouseScroll: "gWHTFindScroll.onScroll(event);",
+		onmouseover: "document.getAnonymousElementByAttribute(document.querySelector('#searchbar').searchButton, 'anonid', 'searchbar-popup').openPopupAtScreen(event.screenX, event.screenY, true); SearchEngine.popupClick(event);",
 	}), searchGoBtn);
 
 	gWHTFindBtn.addEventListener("click", function(event) {
@@ -173,10 +175,11 @@ SearchEngine-button
 
 	var SearchEngineBtn = searchGoBtn.parentNode.insertBefore($C("image", {
 		id: "SearchEngine-button",
-		tooltiptext: "左鍵：Google 加密\n中鍵：Google 翻譯\n右鍵：Google 加密站內\n向上滾動：百度圖片\n向下滾動：Google 圖片\n\n❖ 若搜索欄有關鍵字，便搜尋搜索欄關鍵字\n❖ 若搜索欄沒有關鍵字並選取了文字，便搜尋選取文字\n❖ 否則便貼上就搜尋",
+		tooltiptext: "在選單上\n選擇指定搜索引擎後即搜尋 (新分頁前景)\n❖ 若搜索欄有關鍵字，便搜尋搜索欄關鍵字\n❖ 若搜索欄沒有關鍵字並選取了文字，便搜尋選取文字\n❖ 否則便貼上就搜尋\n\n在按鈕上\n左鍵：Google 加密\n中鍵：Google 翻譯\n右鍵：Google 加密站內\n向上滾動：百度圖片\n向下滾動：Google 圖片\n\n❖ 搜尋搜索欄關鍵字\n❖ 搜尋選取文字\n❖ 貼上就搜尋\n❖ 新分頁前景",
 		style: "list-style-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACo0lEQVQ4jY3Mz2vTcBgG8FdnallK3VKW/mAuLLD1B8OlI23arW22NGWHUNMcwkAHybZT0a21uq60PWQDxzwNetCL9KJ40FvBg55E/wTR01BBEMSDB0Xc6vZ6Sike5r7wuTzf530AAMCyrPP1ev2apmlPFUV5eRpN0540m80CAJwD+9g0zcfhcBhDoRCGw+FThUIhjEQiaJpmCwAAGo2GHgwGcWJiomdqaupYFMWPkiS9TSaTP/r/bJFIBGu1mgT5fP4Ry7Jok2X51dbW1rSu6wOtVuuiZVkuwzBucRz3q7/HsiwqirIPkiR1GIZBhmFwfn7+tWVZg2tra9VMJvNZEISfuVzuRbPZDBaLxcLk5OSx3WUYBmVZboMoip1AIIDj4+Mn1WqVX11dvT06OoqBQKAnHo9/2tvbuyRJ0vP+fGFhoQ2pVKrj9XpxZmbmi67rA/F4/MDr9WI/mqZxeXn5uq7rN2ma7uXpdLoNyblkh6Io5DjuQNf1gWg0+o2iKPyXqqo3TNM0+7NUKtWGRCLRcbvdyLLs793dXU86nX7mdruxn9/v725sbEwtLi7u9+eJRKINsVisQ5IkkiSJ+Xx+u9FoXOY47p3L5UKSJNHn8x0pinIHAM4LgvDe7pIkiYIgtIHn+Y7T6USn04kjIyNHhmGsWJY1aBjGVVVVV0qlUtCyrAsAAOvr69Msy361+70Bh8OBNpIkT3ief6Oqam1paakoy/IDnuc/lMvlJABAuVzm/H7/ocPhwFgs1oZMJvOQIAj8n7Gxse+bm5tzOzs7MZ/Pd0gQBIqieA8qlUrW5XKdnGXE4/F0aZruEgSBw8PDfyqVShQAADRN2x4aGjo+ywhBEEhRVLdQKJSg/9VqtVg2m707Ozt7/zS5XG67Xq9fse/+AnDURgQylYErAAAAAElFTkSuQmCC); margin-right: 1px;",
 		onclick: "SearchEngine.onClick(event);",
 		onDOMMouseScroll: "SearchEngine.onScroll(event);",
+		onmouseover: "document.getAnonymousElementByAttribute(document.querySelector('#searchbar').searchButton, 'anonid', 'searchbar-popup').hidePopup();",
 	}), searchGoBtn);
 
 	var SE = new Array();
@@ -202,7 +205,7 @@ SearchEngine-button
 				}
 				setTimeout(function(selectedEngine) {
 					searchbar.doSearch(text, 'tab');
-//					searchPopup.querySelectorAll("#" + selectedEngine.id)[0].click();
+					searchPopup.querySelectorAll("#" + selectedEngine.id)[0].click();
 				}, 10, searchPopup.querySelector("*[selected=true]"))
 			}
 			var closeES = function() {
