c9962e179e139aa878aa632b2b190d83b9834050
Drager-oos <Drager-oos@users.noreply.github.com>
2014-07-19 12:10:04 +0800

    Update ShowDateAndTime.uc.js

--- BtnPlus/ShowDateAndTime.uc.js
+++ BtnPlus/ShowDateAndTime.uc.js
@@ -1,14 +1,17 @@
+// ==UserScript==
+// @name           ShowDateAndTime.uc.js
+// @homepageURL    https://github.com/Drager-oos/userChrome/blob/master/BtnPlus/ShowDateAndTime.uc.js
+// ==/UserScript==
 (function() {
-	DaT = $("TabsToolbar").appendChild($C("label", {
+	DaT = $("TabsToolbar").appendChild($C("button", {
 				id: "Clock",
-				tooltiptext: "現時日期和時間",
-				style: "margin: 5px -4px 5px 4px; font-weight: bold; font: 18px Arial, Microsoft Yahei; color: grey; min-width: 210px;",
+				style: "margin: 0px -14px 0px 0px; font-weight: bold; font: 18px Microsoft Yahei; color: #FFF; min-width: 84px; -moz-appearance: none; background: #3B3B3B; border: none; padding: 0px; opacity:0.95;",
 				ordinal: "0",
 			}));
 	function startTime() {
 //		var dayName = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-//		var dayName = new Array ("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
-		var dayName = new Array ("週日", "週一", "週二", "週三", "週四", "週五", "週六");
+		var dayName = new Array ("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+//		var dayName = new Array ("週日", "週一", "週二", "週三", "週四", "週五", "週六");
 		var today = new Date();
 		var D = today.getDate(),
 			M = today.getMonth() + 1,
@@ -16,12 +19,11 @@
 			h = today.getHours(),
 			m = today.getMinutes(),
 			s = today.getSeconds();
-		D = checkTime(D);
-		M = checkTime(M);
 		h = checkTime(h);
 		m = checkTime(m);
 		s = checkTime(s);
-		DaT.value = D + "/" + M + "/" + Y + ", " + dayName[today.getDay()] + " " + h + ":" + m + ":" + s;
+		$("Clock").setAttribute("tooltiptext", "現時日期和時間：\n" + Y + " 年 " + M + " 月 " + D + " 日, " + dayName[today.getDay()]);
+		DaT.label = h + ":" + m + ":" + s;
 		setTimeout(function() {startTime();}, 100);
 	}
 	function checkTime(i) {
